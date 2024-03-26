3f0bc98dde6b76f374b3b666c809f7bae97a9dac
Satria-WS <extencezone02@gmail.com>
2024-03-26 12:34:09 +0700

    update sand

---
+++ src/components/Sand.tsx
@@ -0,0 +1,11 @@
+import "../styles/Sand.css";
+const Sand = () => {
+  return (
+    <>
+      <div className="sand-box">
+        <div className="RedSand">RedSand</div>
+      </div>
+    </>
+  );
+};
+export default Sand;
